15e69d7dc07da69b51bcdbfbaec8a921ec3ed1f9
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-08-26 18:12:41 +0530

    complete

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const home = document.getElementsByClassName("home")[0];
 
 const container = document.getElementsByClassName("card-holder")[0];
 
+let modal = document.getElementsByClassName("modal")[0];
+
+
+let localArr = [];
+
 menu.addEventListener("click", (e) => {
     e.preventDefault();
     banner.classList.add("hide");
@@ -24,7 +29,8 @@ async function getMenu() {
     let response = await fetch(url);
     let data = await response.json();
 
-    console.log(data);
+
+    localArr = data;
 
     addDataToUI(data);
 }
@@ -32,8 +38,7 @@ async function getMenu() {
 getMenu();
 
 
-function addDataToUI(data)
-{
+function addDataToUI(data) {
     data.forEach((ele) => {
 
         let div = document.createElement("div")
@@ -52,10 +57,95 @@ function addDataToUI(data)
         </div>
 
         <div class="add-icon">
-            <button>+</button>
+            <button  class="oder" id="${ele.id}">+</button>
         </div>
      </div>`
 
-     container.appendChild(div);
+        container.appendChild(div);
     })
+
+    const add = document.getElementsByClassName("oder");
+
+
+    Array.from(add).forEach((val) => {
+        val.addEventListener("click", takeOrder);
+    })
+
+}
+
+function takeOrder(e) {
+    let id = parseInt(e.target.id);
+
+    let amount = localArr[id - 1].price;
+    let idName = localArr[id - 1].name;
+
+    modal.style.display = "flex";
+    modal.innerHTML = `<div class="modal-container">
+    <h3>Nice Choice!</h3>
+
+    <form>
+        <label for="payment">Pay Your Amount</label>
+        <input type="number" id="amount" placeholder="$${amount}/- Your Payble Amount" required>
+        <button class="pay-btn">Pay</button>
+    </form>
+    <button class="cancel-btn">Cancel</button>
+</div>`
+
+    const cancel = document.getElementsByClassName("cancel-btn")[0];
+
+    cancel.addEventListener("click", () => {
+
+        let a = confirm("Please Pay Your Amount");
+
+        if (a == false) {
+            modal.style.display = "none";
+            alert(`Please Select Your Meal`);
+        }
+
+       
+
+    })
+
+    let isPay = false;
+
+    const form = document.getElementsByTagName("form")[0];
+
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+        isPay = true;
+        
+        modal.style.display = "none";
+
+        OrderPrep(id);
+    })
+}
+
+function OrderPrep(id)
+{
+    let idName = localArr[id - 1].name;
+    
+    setTimeout(() => {
+        let a = alert(`Thanks for choosing ${idName}. Your Payment was accepted. Your order is start preparing`);
+        
+        if(a==undefined)
+        {
+            thankYou(id);
+        }
+        
+    }, 2000)
+
+    
+    
+}
+
+function thankYou(id)
+{
+    let idName = localArr[id - 1].name;
+
+    setTimeout(() => {
+        alert(`Your ${idName} is ready it will be delivered in 10mins. Enjoy Your Meal!`);
+    }, 2500)
+
+    
 }
